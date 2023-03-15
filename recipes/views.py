60862557c5a0b7c5c00632466e910095b7cbac4b
Eduardo Peluchi <eduardopeluchi@outlook.com>
2023-03-14 21:19:48 -0400

    Learning Templates and Render also how to use naming spacing

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,9 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('HOME')
+    return render(request, 'recipes/home.html')
 
 
 def contact(request):
