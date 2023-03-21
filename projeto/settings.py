c089570117ea3e9bb3c55d0b143598ca6c9b85e4
Eduardo Peluchi <eduardopeluchi@outlook.com>
2023-03-21 19:34:11 -0400

    STATIC_ROOT configuration

--- projeto/settings.py
+++ projeto/settings.py
@@ -123,6 +123,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'base_static',
 ]
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
